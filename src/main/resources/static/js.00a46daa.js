5de8e2fe7e9bb5534164ba69634b8be346bb6c18
Sarfaraz-Hussain <call2me786@gmail.com>
2023-04-26 02:58:23 +0530

    fixed the not selected text bug

--- src/main/resources/static/js.00a46daa.js
+++ src/main/resources/static/js.00a46daa.js
@@ -331,7 +331,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50231" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62039" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
